a8b1c56e75a3acc97d7729ee7e53aa636e1d146b
Germano Costa Neto <germano.cneto@gmail.com>
2020-04-10 13:11:07 -0500

    Update cvrandom.R

--- cvrandom.R
+++ cvrandom.R
@@ -1,34 +1,33 @@
-CV.random <- function(gids,ngids,envs=NULL,f.train=.7, df, rep=20, seed=NULL,out.env=NULL){
-  CV1<-Sampling.CV1(gids = gids,ngids = ngids,f = f.train,seed = seed,rep = rep)
-  CV2<-Sampling.CV2(f = f.train,Y = df,seed = seed,rep = rep)
-  if(is.null(out.env)) out.env <- 1  # one-environment-out per fold
-  CV0<-Sampling.CV0(gids=gids,envs=envs,f=f.train,rep=rep,seed=seed,out.env=out.env)
-  return(list(CV1=CV1,CV2=CV2,CV0=CV0))
-}
 
-Sampling.CV1 <- function(gids,ngids=NULL,f=NULL,seed=NULL,rep=NULL){
+Sampling.CV1 <- function(gids,ngids=NULL,nenv=5,f=NULL,seed=NULL,rep=NULL,gidlevel=TRUE){
   if(is.null(seed)) seed <-1010231
   if(is.null(rep)) rep<-10
   if(is.null(f)) f <-.7
   if(is.null(ngids)) ngids <- length(unique(gids))
   n <- f*ngids
   set1 <- matrix(NA,nrow=n,ncol=rep)
+  set2 <- list()
   set.seed(seed)
   for(s in 1:rep) set1[,s] <- sort(sample(1:ngids, size = n , replace = FALSE));set.seed(seed+s)
-  return(set1)
+  if(isFALSE(gidlevel)){
+    for(s in 1:rep) set2[[s]] <-which(gids %in% unique(gids)[set1[,s]])
+    return(set2)
+  }else{
+    return(set1)
+  }
+
 }
 
-Sampling.CV2 <- function(f,Y,seed=NULL,rep=NULL){
+Sampling.CV2 <-function(f,Y,seed=NULL,rep=NULL){
   if(is.null(seed)) seed <-1010231
   if(is.null(rep)) rep<-10
   if(is.null(f)) f <-.7
   n <- nrow(Y)
-  set1 <- matrix(NA,nrow=n*f,ncol=rep)
+  set1 <- list()
   set.seed(seed)
-  for(s in 1:rep) set1[,s] <- sort(sample(1:n, size = n*f , replace = FALSE));set.seed(seed+s)
+  for(s in 1:rep) set1[[s]] <- sort(sample(1:n, size = n*f , replace = FALSE));set.seed(seed+s)
   return(set1)
 }
-
 Sampling.CV0 <- function (gids, envs, ngids, f, out.env, seed, rep) 
 {
   envs <- as.factor(envs)
